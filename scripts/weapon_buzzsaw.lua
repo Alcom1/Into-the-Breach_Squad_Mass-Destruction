970a3bcee0c80002475701f335a8cb537cef0afd
Alex Bronner <acbronn@gmail.com>
2026-08-19 01:05:21 -0400

    Buzzsaw V2

--- scripts/weapon_buzzsaw.lua
+++ scripts/weapon_buzzsaw.lua
@@ -1,14 +1,14 @@
 Weapon_MD_Prime_Buzzsaw = Skill:new{
     Name = "Central Buzzsaw",
-    Description = "Charge forward, dealing damage and spreading A.C.I.D. and fire.",
+    Description = "Pass through a tile, dealing 1 damage to it and spreading its A.C.I.D. or fire status ahead.",
     Class = "Prime",
 	Icon = "advanced/weapons/Science_KO_Crack.png",
     Damage = 1,
-    Harvest = false,
+    Backdraft = false,
     PowerCost = 0,
     Upgrades = 2,
     UpgradeCost = { 2, 2 },
-    UpgradeList = { "Harvest", "+1 Damage" },
+    UpgradeList = { "Backdraft", "+1 Damage" },
     DamageSound = "/mech/distance/artillery/death",
     TipImage = {
         Unit = Point(2, 4),
@@ -19,93 +19,93 @@ Weapon_MD_Prime_Buzzsaw = Skill:new{
     }
 }
 
---Ally Immune upgrade
 Weapon_MD_Prime_Buzzsaw_A = Weapon_MD_Prime_Buzzsaw:new{
-    UpgradeDescription = "Gather A.C.I.D and fire from adjacent tiles before moving.",
-    Harvest = true
+    UpgradeDescription = "Spread A.C.I.D and fire backwards as well as forwards.",
+    Backdraft = true
 }
 
---Damage ramp upgrade
 Weapon_MD_Prime_Buzzsaw_B = Weapon_MD_Prime_Buzzsaw:new{
     UpgradeDescription = "Increases damage by 1.",
 	Damage = 2
 }
 
---Both upgrades combined
 Weapon_MD_Prime_Buzzsaw_AB = Weapon_MD_Prime_Buzzsaw:new{
-    Harvest = true,
+    Backdraft = true,
 	Damage = 2
 }	
 
---Target Area for pass-through
 function Weapon_MD_Prime_Buzzsaw:GetTargetArea(p1)
     local ret = PointList()
-    for i = DIR_START, DIR_END do                           --For each direction
-        for k = 1, INT_MAX do                               --For each tile in a line
-            local point = p1 + DIR_VECTORS[i] * k
-            if not Board:IsValid(point) then                --Break when we leave the board
-                break
-            end
+    for i = DIR_START, DIR_END do
+        local point = p1 + DIR_VECTORS[i] * 2
 
-            if not Board:IsBlocked(point, PATH_FLYER) then  --Point is valid if it can be flown to, if it is empty
-                ret:push_back(point)
-            end
+        if Board:IsValid(point) and not Board:IsBlocked(point, PATH_FLYER) then
+            ret:push_back(point)
         end
     end
 
     return ret
 end
 
---Skill Effect for charge, damage, pull, and upgrades
+local function MD_HiddenArtillery(effect, p)
+	effect:AddScript([[
+		local effect = SkillEffect()
+        local damage = SpaceDamage(Point(]].. p.x ..",".. p.y ..[[), 0)
+        effect:AddArtillery
+		Board:AddEffect(effect)
+	]])
+end
+
 function Weapon_MD_Prime_Buzzsaw:GetSkillEffect(p1, p2)
     local ret = SkillEffect()
-    local damagePoints = p1:MD_Bresenham(p2, 1, 1)              --Points from here to there
-    local selfPawn = Board:GetPawn(p1)
 
-    local isFire = selfPawn:IsFire() or Board:IsFire(p1)
-    local isAcid = selfPawn:IsAcid() or Board:IsAcid(p1)
-
-    if self.Harvest then
-        for dir = DIR_START, DIR_END do
-
-            local harvestPoint = p1 + DIR_VECTORS[dir]
-
-            if dir ~= GetDirection(p2 - p1) then
-
-                if Board:IsFire(harvestPoint) then
-                    isFire = true
-                end
-
-                if Board:IsAcid(harvestPoint) then
-                    isAcid = true
-                end
-            end
-        end
-    end
-    
     ret:AddSound(self.DamageSound)                              --Initial Saw Sound
     ret:AddCharge(Board:GetPath(p1, p2, PATH_FLYER), NO_DELAY)  --Charge!
 
-    for i, point in ipairs(damagePoints) do
+    ret:AddDelay(0.1)
 
-        local damage = SpaceDamage(point, self.Damage)          --Damage
-        damage.sSound = self.DamageSound
+    local pMid = p1 + DIR_VECTORS[GetDirection(p2 - p1)]
 
-        if Board:IsFire(point) then
-            isFire = true
+    local pawnMid = Board:GetPawn(pMid)
+    local isFire = Board:IsFire(pMid) or (pawnMid ~= nil and pawnMid:IsFire())
+    local isAcid = Board:IsAcid(pMid) or (pawnMid ~= nil and pawnMid:IsAcid())
+
+    local damage = SpaceDamage(pMid, self.Damage)
+    --damage.iAcid = 1
+    damage.sSound = self.DamageSound
+
+    ret:AddDamage(damage)
+    ret:AddBounce(pMid, 1)
+
+    local distance = 3
+    while true do
+
+        local p3 = p1 + DIR_VECTORS[GetDirection(p2 - p1)] * distance
+
+        if not Board:IsValid(p3) then
+            break
         end
 
-        if Board:IsAcid(point) then
-            isAcid = true
+        if isAcid then
+            local damage2 = SpaceDamage(p3, 0)
+            damage2.iAcid = 1
+            damage2.sAnimation = "ExploAcid1"
+            damage2.sSound = "/impact/generic/acid_canister"
+            ret:AddSound("/enemy/beetle_1/attack_impact")
+            ret:AddArtillery(p2, damage2, "effects/shotup_ant2.png", NO_DELAY)
         end
 
-        damage.iFire = isFire and 1 or 0
-        damage.iAcid = isAcid and 1 or 0
+        if isFire then
+            local damage2 = SpaceDamage(p3, 0)
+            damage2.iFire = 1
+            damage2.sAnimation = "ExploArt2"
+            ret:AddSound("/weapons/fireball")
+            ret:AddArtillery(p2, damage2, "effects/shotup_ignite_fireball.png", NO_DELAY)
+        end
 
-        ret:AddDamage(damage)                                   --Damage
-        ret:AddBounce(point, 1)                                 --Bounce
-        
-        ret:AddDelay(0.1)                                       --Delay effects as we travel
+        ret:AddDelay(0.1)
+
+        distance = distance + 1
     end
 
     return ret
